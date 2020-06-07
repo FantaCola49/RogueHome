974bca32767b10d3c929c42c81e1c47a4316032b
FantaCola49 <49249521+FantaCola49@users.noreply.github.com>
2020-06-07 14:04:34 +0300

    Update OpenDoor script
    Улучшил стилистику кода согласно рекомендациям

--- Assets/Scripts/Environment/OpenDoor.cs
+++ Assets/Scripts/Environment/OpenDoor.cs
@@ -3,34 +3,36 @@
 public class OpenDoor : MonoBehaviour
 {
     private AudioSource _audio;
-    private bool _isIN; // _isIN ~ Rogue entered the house?
+    private bool _RogueEntered;
+    private int _VolumeRate = 5;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.TryGetComponent<Rogue>(out Rogue rogue))
         {
-            if (!_isIN)
+            if (!_RogueEntered)
             {
-                _isIN = true;
+                _RogueEntered = true;
                 _audio.Play();
             }
             else
-                _isIN = false;
+            {
+                _RogueEntered = false;
+            }
 
             Debug.Log("The Rogue went through the door!!!");
         }
     }
 
-    // Volume change depends on deltatime and _isIN bool trigger
     private void Update()
     {
-        if (_isIN)
+        if (_RogueEntered)
         {
-            _audio.volume += Time.deltaTime/10;
+            _audio.volume += (Time.deltaTime / _VolumeRate);
         }
         else
         {
-            _audio.volume -= Time.deltaTime / 10;
+            _audio.volume -= (Time.deltaTime / _VolumeRate);
         }
         if (_audio.volume == 0)
             _audio.Stop();
